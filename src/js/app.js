98084870a434387475e9465880ed910ba520d354
rvitality <vital.renzjohn@gmail.com>
2021-04-26 20:22:17 -0700

    Finished the landing section parallax feature.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,21 @@
-import "../scss/app"
+const translates = document.querySelectorAll(".translate");
 
-console.log("worlks");
+const header = document.querySelector(".header");
+const headerTitle = document.querySelector(".header__title");
+const headerBottomFade = document.querySelector(".header__bottom-fade");
+
+const headerHeight = header.offsetHeight;
+
+window.addEventListener("scroll", () => {
+    let scrollFromTop = window.pageYOffset;
+
+    translates.forEach(el => {
+        let speed = el.dataset.speed;
+        el.style.transform = `translateY(${scrollFromTop * +speed}px)`;
+    });
+
+    headerTitle.style.opacity = -scrollFromTop / (headerHeight / 2) + 1;
+    headerBottomFade.style.height = `${scrollFromTop * .05 + 15}vh`;
+
+});
+
